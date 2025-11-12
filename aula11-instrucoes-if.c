29faab9dd451eab83ac8d55a4eb6eb81a92acf14
GiuliadBem <giuliadbem@gmail.com>
2025-11-12 16:39:55 -0300

    Aulas 9-10

---
+++ aula11-instrucoes-if.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  
+  // if statement = Do some code if a condition is true.
+  //                If the condition is false, don't do it.
+
+  int age = 12;
+
+  if(age >= 18){
+    printf("You are an adult");
+  }
+  else{
+    printf("You are a child");
+  }
+  
+  return 0;
+}
